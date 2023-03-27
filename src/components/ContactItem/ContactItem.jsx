16d6ac0c30bbe33ef7f86f7af8ca02880df9c013
Artem <113770041+Artemdream@users.noreply.github.com>
2023-03-27 11:31:41 +0300

    change maplist

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,22 +1,20 @@
 import PropTypes from 'prop-types';
 import '../ContactItem/ContactItem.css';
 
-export const ContactItem = ({ contacts, deleteContact }) => {
-  return contacts.map(({ id, name, number }) => (
-    <li key={id} className="contact-item">
-      <p>
-        {name}: {number}
-      </p>
-      <button
-        className="item-btn"
-        type="button"
-        onClick={() => deleteContact(id)}
-      >
-        Delete
-      </button>
-    </li>
-  ));
-};
+export const ContactItem = ({ id, name, number, deleteContact }) => (
+  <li className="contact-item">
+    <p>
+      {name}: {number}
+    </p>
+    <button
+      className="item-btn"
+      type="button"
+      onClick={() => deleteContact(id)}
+    >
+      Delete
+    </button>
+  </li>
+);
 
 ContactItem.propTypes = {
   contacts: PropTypes.arrayOf(
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types';
 import { ContactItem } from '../ContactItem/ContactItem';
 
-export const ContactList = ({ contacts, onDeleteContact }) => {
-  return (
-    <ul>
-      <ContactItem contacts={contacts} deleteContact={onDeleteContact} />
-    </ul>
-  );
-};
+export const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul>
+    {contacts.map(({ id, name, number }) => (
+      <ContactItem
+        key={id}
+        id={id}
+        name={name}
+        number={number}
+        deleteContact={onDeleteContact}
+      />
+    ))}
+  </ul>
+);
 
 ContactList.propTypes = {
   contacts: PropTypes.array.isRequired,
